a317278e4ce0f838e79f0ec5bac0fd57c26c138e
amit <amitmakwana7563@gmail.com>
2025-05-10 18:35:25 +0530

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import Counter from "@/components/Counter";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 // Demo counter for redux
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/register");
+  }, [])
+
   return (
-    <div className="min-h-screen p-8 bg-gray-100 space-y-6">
-      <Counter />
-    </div>
+    <div className="min-h-screen p-8 bg-gray-100 space-y-6" >
+      {/* <Counter /> */}
+    </div >
   );
 }
